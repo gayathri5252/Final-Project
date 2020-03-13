3509c39a20469625851d6be109d4d7e7105de554
gayathri5252 <vgayathri@5252@gmail.com>
2020-03-13 17:59:50 +0530

    first commit

---
+++ Angular/eMart/src/app/seller-item/seller-itemdisplay/seller-itemdisplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seller-itemdisplay',
+  templateUrl: './seller-itemdisplay.component.html',
+  styleUrls: ['./seller-itemdisplay.component.css']
+})
+export class SellerItemdisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
